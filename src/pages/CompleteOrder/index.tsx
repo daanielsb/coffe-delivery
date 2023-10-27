8d38326bf0fe8beef7eaf586530a98ea91004556
daanielsb <daanielsb@hotmail.com>
2023-10-27 08:54:22 -0300

    CompleteOrderContainer

--- src/pages/CompleteOrder/index.tsx
+++ src/pages/CompleteOrder/index.tsx
@@ -1,3 +1,9 @@
+import { CompleteOrderContainer } from "./style";
+
 export function CompleteOrderPage() {
-    return <h1>Complete</h1>
+    return (
+        <CompleteOrderContainer className="container">
+            Oi
+        </CompleteOrderContainer>
+        )
 }
