07b06ba29e9133c77c385de73ad93765ff48a851
daanielsb <daanielsb@hotmail.com>
2023-11-01 22:09:40 -0300

    Coffe Cart Styles

--- src/pages/CompleteOrder/components/CoffeeCartCard/styles.ts
+++ src/pages/CompleteOrder/components/CoffeeCartCard/styles.ts
@@ -1,6 +1,30 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const CoffeCartCardContainer = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    border-bottom: 1px solid ${({theme})=> theme.colors["base-button"]};
+    padding-bottom: 1.5rem;
+    margin-bottom: 1.5rem;
+
+
+    > div {
+        display: flex;
+        align-items: center;
+        gap: 1.25rem;
+        img{
+            width: 4rem;
+            height: 4rem;
+        }
+    }
+
+    > p{
+        align-self: flex-start;
+        font-weight: 700;
+    }
 
 `
 
