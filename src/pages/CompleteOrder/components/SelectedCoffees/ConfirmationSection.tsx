e5e8377770d3f47aefdd749cd282f648264da8cb
daanielsb <daanielsb@hotmail.com>
2023-11-06 08:58:49 -0300

    Button

--- src/pages/CompleteOrder/components/SelectedCoffees/ConfirmationSection.tsx
+++ src/pages/CompleteOrder/components/SelectedCoffees/ConfirmationSection.tsx
@@ -1,3 +1,4 @@
+import { Button } from "../../../../components/Button";
 import { RegularText } from "../../../../components/typography";
 import { ConfirmationSectionContainer } from "./styles";
 
@@ -6,9 +7,18 @@ export function ConfirmationSection(){
         <ConfirmationSectionContainer>
             <div>
                 <RegularText size="s">Total de itens</RegularText>
-                <RegularText>R$9,90</RegularText>
-                
+                <RegularText>R$9,90</RegularText>                
             </div>
+            <div>
+                <RegularText size="s">Entrega</RegularText>
+                <RegularText>R$3,50</RegularText>                
+            </div>
+            <div>
+                <RegularText weight='700' color="subtitle" size="l">Total</RegularText>
+                <RegularText weight='700' color="subtitle" size="l">R$ 29,90</RegularText>
+            </div>
+
+            <Button text="Confirmar Pedido"/>
             
         </ConfirmationSectionContainer>
     )
