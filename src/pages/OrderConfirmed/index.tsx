55aac2e3f5b016fb210de153ac766029004b5f29
daanielsb <daanielsb@hotmail.com>
2023-11-07 16:49:36 -0300

    OrderConfirmed Page

---
+++ src/pages/OrderConfirmed/index.tsx
@@ -0,0 +1,9 @@
+import { OrderConfirmedContainer } from "./styles";
+
+export function OrderConfirmedPage(){
+    return(
+        <OrderConfirmedContainer className="container">
+            <p>oi</p>
+        </OrderConfirmedContainer>
+    )
+}
